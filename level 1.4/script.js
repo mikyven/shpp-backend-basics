12612fe9547e1603377a7f0b77c37be177d3009c
mikyven <alexpry09@gmail.com>
2024-08-25 23:48:49 +0300

    finished level 1.4

--- level 1.4/script.js
+++ level 1.4/script.js
@@ -112,12 +112,184 @@ switchInput.addEventListener("input", () =>
 );
 
 // task 8
-const taskDiv = document.querySelector(".task-8");
+const taskEightDiv = document.querySelector(".task-8");
 const imageInput = document.querySelector(".image-input");
 const showImageButton = document.querySelector(".show-image-button");
 
 showImageButton.addEventListener("click", () => {
+  const url = imageInput.value;
+  if (!url) return;
   const image = document.createElement("img");
-  image.src = imageInput.value;
-  taskDiv.appendChild(image);
+  image.src = url;
+  taskEightDiv.appendChild(image);
+});
+
+// task 9
+const taskNineDiv = document.querySelector(".task-9");
+const imageTextarea = document.querySelector(".image-textarea");
+const showImagesButton = document.querySelector(".show-images-button");
+
+showImagesButton.addEventListener("click", () => {
+  const urls = imageTextarea.value.split("\n");
+  if (!urls.length) return;
+  urls.forEach((i) => {
+    const image = document.createElement("img");
+    image.src = i;
+    taskNineDiv.appendChild(image);
+  });
+});
+
+// task 10
+const coordinates = document.querySelectorAll("#coordinate");
+const updateCoordinates = (e) => {
+  coordinates.forEach((i) => {
+    i.textContent = `${i.className}: ${e[`client${i.className}`]}`;
+  });
+};
+
+document.addEventListener("mousemove", updateCoordinates);
+
+// task 11
+const languageDiv = document.querySelector(".selected-language");
+const displayLanguage = () => {
+  languageDiv.textContent = `Language: ${navigator.language}`;
+};
+
+// task 12
+const latitudeDiv = document.getElementById("latitude");
+const longitudeDiv = document.getElementById("longitude");
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+
+      latitudeDiv.textContent = lat;
+      longitudeDiv.textContent = long;
+    });
+  } else {
+    latitudeDiv.parentElement.textContent =
+      "Geolocation is not supported in your browser.";
+  }
+};
+
+// task 13
+const localInput = document.querySelector(".local-input");
+const cookieInput = document.querySelector(".cookie-input");
+const sessionInput = document.querySelector(".session-input");
+
+localInput.addEventListener("input", (e) =>
+  localStorage.setItem("localInputValue", e.target.value)
+);
+
+cookieInput.addEventListener("input", (e) => {
+  document.cookie = `cookieInputValue=${e.target.value}`;
+});
+
+sessionInput.addEventListener("input", (e) => {
+  sessionStorage.setItem("sessionInputValue", e.target.value);
+});
+
+window.addEventListener("load", (e) => {
+  displayLanguage();
+  getLocation();
+
+  localInput.value = localStorage.getItem("localInputValue") || "";
+  cookieInput.value = document.cookie.split("=")[1] || "";
+  sessionInput.value = sessionStorage.getItem("sessionInputValue") || "";
+});
+
+// task 14
+const scrollButton = document.querySelector(".scroll-button");
+
+scrollButton.addEventListener("click", () =>
+  window.scroll({ top: 0, behavior: "smooth" })
+);
+
+document.addEventListener("scroll", () => {
+  const doc = document.documentElement;
+  const isAtTheBottom =
+    doc.clientHeight + window.scrollY >= (doc.scrollHeight || doc.clientHeight);
+  if (isAtTheBottom) {
+    scrollButton.classList.remove("hidden");
+  } else if (!isAtTheBottom && !scrollButton.classList.contains("hidden")) {
+    scrollButton.classList.add("hidden");
+  }
+});
+
+// task 15
+const innerBlock = document.querySelector(".inner-block");
+const outerBlock = document.querySelector(".outer-block");
+
+innerBlock.addEventListener("click", (e) => {
+  e.stopPropagation();
+  alert("Hello from inner block");
+});
+
+outerBlock.addEventListener("click", () => {
+  alert("Hello from outer block");
+});
+
+// task 16
+const showBlockButton = document.querySelector(".show-block-button");
+const blockingBackground = document.querySelector(".blocking-background");
+
+showBlockButton.addEventListener("click", () => {
+  blockingBackground.classList.remove("hidden");
+});
+
+blockingBackground.addEventListener("click", () => {
+  blockingBackground.classList.add("hidden");
+});
+
+// task 17
+const unreloadingForm = document.querySelector(".task-17 form");
+
+unreloadingForm.addEventListener("submit", (e) => e.preventDefault());
+
+// task 18
+const fileLabel = document.querySelector(".file-label");
+const fileInput = document.querySelector(".file-input");
+const result = document.querySelector(".result");
+
+fileLabel.addEventListener("dragenter", (e) => {
+  e.preventDefault();
+  fileLabel.classList.add("hovered");
+});
+
+fileLabel.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  if (e.target === fileLabel && !fileLabel.contains(e.relatedTarget)) {
+    fileLabel.classList.remove("hovered");
+  }
+});
+
+fileLabel.addEventListener("dragover", (e) => e.preventDefault());
+
+function getTotalFilesSizes(items) {
+  let total = 0;
+  if (items) {
+    [...items].forEach((item) => {
+      if (item.kind === "file") {
+        total += +(item.getAsFile().size / 1024 ** 2).toFixed(2);
+      } else {
+        total += +(item.size / 1024 ** 2).toFixed(2);
+      }
+    });
+  }
+  return total;
+}
+
+fileLabel.addEventListener("drop", (e) => {
+  e.preventDefault();
+  result.textContent = `Total size of all files is ${getTotalFilesSizes(
+    e.dataTransfer.items
+  )}MB.`;
+});
+
+fileInput.addEventListener("change", (e) => {
+  e.preventDefault();
+  result.textContent = `Total size of all files is ${getTotalFilesSizes(
+    e.target.files
+  )}MB.`;
 });
